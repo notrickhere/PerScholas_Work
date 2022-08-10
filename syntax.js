808a252deba4df1c7e343a4fea1d88ab1162fe1c
notrickhere <84467942+notrickhere@users.noreply.github.com>
2022-08-10 08:53:12 -0400

    Add files via upload

---
+++ syntax.js
@@ -0,0 +1,34 @@
+let a = 4
+let b = 5
+let c = a + b
+console.log(c)
+
+let d = 6
+let e = 7
+let f = d + e
+
+
+let g = f % c
+console.log(g)
+
+let h = 8
+let i = 9
+let j = 10
+let k = 11
+let l = 12
+let firstAvg = (h + i + j + k + l)/5
+
+let m = 13
+let n = 14
+let o = 15
+let p = 16
+let q = 17
+let secAvg = (m + n + o + p + q)/5
+let firAndSecAvg = (firstAvg + secAvg)/2
+console.log(firAndSecAvg)
+
+let r = (4 + 10 + 22 + -30 + 55)/5
+
+let s = (68 + 7 + -22 + 9 + 100)/5
+let bothAvg = (r + s)/2
+console.log(bothAvg)
